27fbdfa69afda35017550b9236cdb7bda7eb832f
MorningStar <113748706+MorningStar2061@users.noreply.github.com>
2022-12-01 20:15:30 +0530

    Add files via upload

---
+++ Assignment 1/A1_Q1.c
@@ -0,0 +1,12 @@
+/*
+Author: Akash Soni.
+Date:15 september
+Aim: first c program
+Language:c
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Hello");
+    return 0;
+}
---
+++ Assignment 1/A1_Q7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Akash Soni");
+    return 0;
+}
